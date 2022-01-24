dc454a97576cba9403aece4fcdb0c208668888e9
CarolynnFleming <cfcatpink@gmail.com>
2022-01-24 18:20:44 -0500

    done

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import './Footer.css'
-
+import './Footer.css' 
 export default function Footer(props) {
   return <footer>Copyright Alchemy Codelab {props.year}</footer>
 }
